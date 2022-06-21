4c8e3e113d127941954c13893da6f44f96f2f39a
Evgeny Mitin <101546611+MEVpro@users.noreply.github.com>
2022-06-21 16:40:33 +0300

    update lesson 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const task = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: false},
+        {id: 3, title: "React", isDone: true},
+        {id: 4, title: "Next", isDone: false}
+    ]
+
+    return (
+        <div className="App">
+           <Todolist
+               title={"What to learn"}
+               task={task}
+           />
+
+        </div>
+    );
 }
 
 export default App;
